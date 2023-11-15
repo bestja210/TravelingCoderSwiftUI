9e2769ca0de7a0fa2121a75e91490442c1ad75b6
Jacob Best <bestja210@gmail.com>
2023-11-15 10:22:29 -0500

    Initial Commit

--- SwiftUIStoryTemplate/ContentView.swift
+++ SwiftUIStoryTemplate/ContentView.swift
@@ -4,26 +4,69 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
+            HomePageView()
+//                .tabItem {
+//                    Label("Home", systemImage: "house.circle")
+//                }
             Chapter1View()
-                .tabItem {
-                    Label("Chapter 1", systemImage: "1.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 1", systemImage: "1.circle")
+//                }
             Chapter2View()
-                .tabItem {
-                    Label("Chapter 2", systemImage: "2.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 2", systemImage: "2.circle")
+//                }
             Chapter3View()
-                .tabItem {
-                    Label("Chapter 3", systemImage: "3.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 3", systemImage: "3.circle")
+//                }
             Chapter4View()
-                .tabItem {
-                    Label("Chapter 4", systemImage: "4.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 4", systemImage: "4.circle")
+//                }
             Chapter5View()
-                .tabItem {
-                    Label("Chapter 5", systemImage: "5.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 5", systemImage: "5.circle")
+//                }
+        }
+        .tabViewStyle(.page)
+        .ignoresSafeArea()
+    }
+}
+
+struct HomePageView: View {
+    var body: some View {
+        ZStack {
+            Image("HomePage")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            VStack {
+                Spacer()
+                Text("The\nTraveling Coder")
+                    .multilineTextAlignment(.center)
+                    .font(.system(
+                        size: 30,
+                        weight: .bold,
+                        design: .serif
+                    ))
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("Team Swift\n")
+                    .font(.system(
+                        size: 25,
+                        weight: .bold,
+                        design: .serif))
+                Text("Artem, Chanel\nJacob, Kyra")
+                    .multilineTextAlignment(.center)
+                    .font(.system(
+                        size: 15,
+                        weight: .bold,
+                        design: .serif))
+                Spacer()
+            }
+            .foregroundStyle(Color.white)
         }
     }
 }
